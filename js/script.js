bf227625e0c04e9c4004008004a5092345d6a39e
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2020-10-25 18:52:29 +0300

    Добавил интерактивную карту

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const contactsLink = document.querySelector(".contacts-button");
 const contactsClose = document.querySelector(".close");
 const popup = document.querySelector(".popup");
 const contactForm = document.querySelector(".contact-form");
+const contactButton = document.querySelector(".send-button");
 const userName = document.querySelector(".name-user");
 const userMail = document.querySelector(".email-user");
 const userMassage = document.querySelector(".message-textarea");
@@ -19,9 +20,10 @@ try {
     isStorageSupport = false;
 }
 
-contactsLink.addEventListener("click", function(evt) {
+contactsLink.addEventListener('click', function(evt) {
     evt.preventDefault();
-    popup.classList.add("popup-show");
+    popup.classList.remove('popup-hidden');
+    popup.classList.add('popup-show');
     if (storageName && storageMail) {
         userName.value = storageName;
         userMail.value = storageMail;
@@ -29,11 +31,22 @@ contactsLink.addEventListener("click", function(evt) {
     } else {
         userName.focus();
     }
+
+    setTimeout(function() {
+        popup.classList.remove('popup-show');
+    }, 1000);
+
 });
 
-contactsClose.addEventListener("click", function() {
-    popup.classList.remove("popup-show");
-    popup.classList.remove("popup-error");
+contactsClose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    popup.classList.add('popup-close');
+
+    setTimeout(function() {
+        popup.classList.add('popup-hidden');
+        popup.classList.remove('popup-close');
+        popup.classList.remove("popup-error");
+    }, 600);
 });
 
 contactForm.addEventListener("submit", function(evt) {
@@ -42,6 +55,10 @@ contactForm.addEventListener("submit", function(evt) {
         popup.classList.remove("popup-error");
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("popup-error");
+
+        setTimeout(function() {
+            popup.classList.remove("popup-error");
+        }, 600);
     } else {
         if (isStorageSupport) {
             localStorage.setItem("name", userName.value);
@@ -50,12 +67,28 @@ contactForm.addEventListener("submit", function(evt) {
     }
 });
 
+contactButton.addEventListener("click", function(evt) {
+    if (!userName.checkValidity() || !userMail.checkValidity()) {
+        popup.classList.remove("popup-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("popup-error");
+        setTimeout(function() {
+            popup.classList.remove("popup-error");
+        }, 600);
+    }
+});
+
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
-        if (popup.classList.contains("popup-show")) {
+        if (!popup.classList.contains("popup-hidden")) {
             evt.preventDefault();
-            popup.classList.remove("popup-show");
-            popup.classList.remove("popup-error");
+            popup.classList.add('popup-close');
+
+            setTimeout(function() {
+                popup.classList.add('popup-hidden');
+                popup.classList.remove('popup-close');
+                popup.classList.remove("popup-error");
+            }, 600);
         }
     }
 });
